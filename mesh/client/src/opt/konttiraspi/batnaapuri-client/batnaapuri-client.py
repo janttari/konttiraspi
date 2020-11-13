032cfb3038fce3d777df60b7e421fccfb51530ee
Jani Janttari <janttari@yandex.ru>
2020-11-13 16:36:05 +0200

    gup

---
+++ mesh/client/src/opt/konttiraspi/batnaapuri-client/batnaapuri-client.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+'''Tämä on asiakas-esimerkki joka ottaa yhteyttä flaskin socket-palvelimelle'''
+import json, time, socketio, threading # sudo pip3 install python-socketio
+from configobj import ConfigObj
+
+config=ConfigObj('/boot/asetukset.txt')
+
+def kyseleNaapurit(): #kyselee batmanin näkemät naapurilaitteet # [('00:c0:ca:98:8f:9f', ' 0.500', ' 7.8'), ('00:c0:ca:98:8e:ed', ' 0.190', '35.6')]
+    #print("OMA IP: "+omaIP, omaMAC)
+    #naapuritraaka = subprocess.getoutput('sudo batctl n -H').split("\n")
+    tmpnaapurit=[]
+    ##
+    jrivi='{"laite": "'+config.get("mesh_name")+'", "data":  ['
+    tmpnaapurit = [('00:c0:ca:98:8f:9f', ' 0.500', ' 7.8'), ('00:c0:ca:98:8e:ed', ' 0.190', '35.6')] #DEMO
+    for t in tmpnaapurit:
+        nmac=t[0]
+        nviive=t[1]
+        nteho=t[2]
+        jrivi+='{"mac": "'+nmac+'", "viive": "'+nviive+'", "teho": "'+nteho+'"},'
+    jrivi=jrivi[:-1]+']}'
+    print(jrivi)
+    ##
+    return jrivi
+    for n in naapuritraaka:
+        nmac = n[0:17]
+        naika = n[20:26]
+        nteho = n[36:40]
+        #print(nmac,naika,nteho)
+        tmpnaapurit.append((nmac,naika,nteho))
+    return tmpnaapurit # [('00:c0:ca:98:8f:9f', ' 0.500', ' 7.8'), ('00:c0:ca:98:8e:ed', ' 0.190', '35.6')]
+
+class WsAsiakas:
+    def __init__(self):
+        self.t=threading.Thread(target=self.wsYhteys)
+        self.t.start()
+    def wsYhteys(self):
+        self.sio=socketio.Client()
+        self.sio.connect(config.get("batnaapuri_server"),namespaces=['/meshraspi'])
+        @self.sio.on('newnumber', namespace='/meshraspi')
+        def my_custom_event(data):
+            print("---", data)
+    def laheta(self, sanoma):
+        jsanoma=json.loads(sanoma)
+        print(jsanoma)
+        self.sio.emit('naapuri_message', sanoma, namespace='/meshraspi')
+
+if __name__ == "__main__":
+    a=WsAsiakas()
+    k=0
+    while True:
+        k+=1
+        print(k)
+        if k%10 == 0 or k== 2:
+            naap=kyseleNaapurit()
+            #print(naap)
+            a.laheta(naap)
+        if k%13 == 0:
+            print("muutos")
+            a.laheta('{"laite": "'+config.get("mesh_name")+'", "data": [{"mac": "00:c0:ca:98:8f:9f", "viive": " 1.500", "teho": " 7.8"}, {"mac": "00:c0:ca:98:8e:ed", "viive": " 0.190", "teho": "35.6"}, {"mac": "00:c0:ca:98:8e:31", "viive": " 0.190", "teho": "22.6"}]}')
+        #a.laheta('{"data": "piip"}')
+        time.sleep(1)
---
+++ mesh/gateway/src/opt/konttiraspi/batnaapuri-server/batnaapuri-server.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python3
+from flask_socketio import SocketIO, emit
+from flask import Flask, render_template, url_for, copy_current_request_context, request
+import time, threading, logging, sys, matplotlib, json
+import networkx as nx #emuloi kontti-raspberryjen MESH-verkkoa jossa raspit raportoi naapuri laitteistaan tälle serverille
+import matplotlib.pyplot as plt
+from configobj import ConfigObj
+
+config=ConfigObj('/boot/asetukset.txt')
+
+meshnaapuridata={} #tässä on clientien ilmoittamien naapurihavaintojen kokonaisuus
+viimmeshnaapuridata={} #tässä on clientien ilmoittamien naapurihavaintojen kokonaisuus viimeinnen tunnettu tila vertailua varten
+
+def luoVisuaali():
+    G = nx.Graph()
+    for isan in meshnaapuridata: #käydään isäntien naapurit läpi
+        print("isanta:",isan)
+        for naap in meshnaapuridata[isan]["naapurit"]:
+            naapuri=naap["laite"]
+            teho=naap["teho"]
+            print(naapuri, teho)
+            G.add_edge(isan[-5:], naapuri[-5:], valimatka=int(40-float(teho)))
+    pos = nx.spring_layout(G, seed=0)
+    plt.figure(figsize =(9, 9))
+    nx.draw(G, pos,with_labels = True, node_color ='blue', node_size=3000, font_size=8, font_color="yellow")
+    edge_labels = nx.get_edge_attributes(G,'valimatka')
+    nx.draw_networkx_edge_labels(G, pos, edge_labels = edge_labels)
+    ##print(nx.shortest_path(G, ’A’, ’K’))
+    ##length = nx.shortest_path_length(G, source=’A’, target=’K’, weight=’valimatka’)
+    ##print("VM",length)
+    plt.savefig('static/kartta.png')
+    f.paivitaKuva()
+
+def vertaaNaapuriMuutoksia(): #onko naapuridatassa tapahtunut muutoksia?
+    global viimmeshnaapuridata
+    muuttunut=False
+    #print("----------------------------")
+    #print(meshnaapuridata)
+    for isanta in meshnaapuridata:
+        for naapurit in meshnaapuridata[isanta]["naapurit"]:
+            if isanta in viimmeshnaapuridata:
+                vanhat=viimmeshnaapuridata[isanta]["naapurit"]
+
+                #print("VABNHAT",vanhat,type(vanhat),len(vanhat))
+                #print("UUUUUUU",naapurit,type(naapurit),len(naapurit))
+                if meshnaapuridata[isanta]["naapurit"] != vanhat:
+                    #print("MUUUUUUUUUUUUUUT")
+                    muuttunut=True
+            else:
+                print("ei ole")
+                muuttunut=True
+    #print("AA",meshnaapuridata)
+    #print("BB,",viimmeshnaapuridata)
+    if muuttunut:
+        print("MUUTTUNUT")
+        viimmeshnaapuridata=meshnaapuridata.copy()
+        print(meshnaapuridata)
+        luoVisuaali()
+
+class FlaskPalvelu:
+    def __init__(self):
+        self.clientsMeshraspi=[]
+        self.clientsSelain=[]
+        self.app = Flask(__name__)
+        #self.app.logger.disabled = True #hide flask messages
+        #log = logging.getLogger('werkzeug') #hide flask messages
+        #log.disabled = True #hide flask messages
+        #cli = sys.modules['flask.cli'] #hide flask messages
+        #cli.show_server_banner = lambda *x: None #hide flask messages
+        self.app.config['SECRET_KEY'] = 'secret!'
+        self.app.config['DEBUG'] = False
+        self.socketio = SocketIO(self.app, async_mode='threading', logger=False, engineio_logger=False)
+        fp=threading.Thread(target=self.palv)
+        fp.start()
+
+        @self.app.route('/')
+        def index():
+            return render_template('index.html')
+
+        @self.socketio.on('connect', namespace='/selain')
+        def selain_connect():
+            self.clientsSelain.append(request.sid)
+            print("selain")
+
+        @self.socketio.on('connect', namespace='/meshraspi')
+        def meshraspi_connect():
+            self.clientsMeshraspi.append(request.sid)
+            print("CON")
+
+        @self.socketio.on('disconnect', namespace='/meshraspi')
+        def test_disconnect():
+            print('Client disconnected')
+
+        @self.socketio.on('naapuri_message', namespace='/meshraspi') #client lähettää tietoa naapureistaan
+        def __receiv_message(data):
+            jdata=json.loads(data)
+            isanta=jdata["laite"]
+            naap=[]
+            print("VVV isanta:",isanta)
+            for j in jdata["data"]:
+                nmac=j["mac"]
+                nteho=j["teho"]
+                nviive=j["viive"]
+                kohde={"laite": nmac, "teho": nteho, "viive": nviive}
+                naap.append(kohde)
+            aika=time.time()
+            meshnaapuridata[isanta]={"naapurit": naap, "havaintoaika": aika}
+            vertaaNaapuriMuutoksia()
+
+        @self.socketio.on('selainmsg', namespace='/selain') #selaimelta tulevia komentoja
+        def __receiv_message(data):
+            print("SSS",data)
+
+    def lahetaMeshraspi(self,number):
+        #for cl in self.clients:
+            #self.socketio.emit('newnumber', {'number': cl}, room=cl, namespace='/meshraspi') #vain pyytäjälle
+        self.socketio.emit('newnumber', {'number': number}, namespace='/meshraspi') #kaikille namespacessa
+
+    def lahetaSelain(self,number):
+        self.socketio.emit('naapuridata', {'data': number}, namespace='/selain') #kaikille namespacessa
+
+    def paivitaKuva(self):
+        print("päivitä selaimen kuva")
+        self.socketio.emit('paivitakuva', {'data': 0}, namespace='/selain') #kaikille namespacessa
+
+    def palv(self):
+        self.socketio.run(self.app, host='0.0.0.0', port=int(config.get("batnaapuri_portti")))
+
+if __name__ == '__main__':
+    f=FlaskPalvelu()
+    num=0
+    while True:
+        num+=1
+        #print(".",meshnaapuridata)
+        f.lahetaSelain("piip"+str(num))
+        time.sleep(1)
